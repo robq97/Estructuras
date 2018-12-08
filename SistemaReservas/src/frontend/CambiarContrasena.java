b7e1d03d44d5b418c8275d35eead1be0b84d38f7
Bryan Corrales Madrigal <bcorrales@ulacit.ac.cr>
2018-12-08 16:44:08 -0600

    2018/12/08

--- SistemaReservas/src/frontend/CambiarContrasena.java
+++ SistemaReservas/src/frontend/CambiarContrasena.java
@@ -139,22 +139,16 @@ public class CambiarContrasena extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CambiarContrasena.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CambiarContrasena.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CambiarContrasena.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(CambiarContrasena.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        
+        //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new CambiarContrasena().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new CambiarContrasena().setVisible(true);
         });
     }
 
